fa8b52587671eb0f52dea7b1f41c94e4df726ce8
LIYONS VARGHESE <liyonsvarghese@gmail.com>
2021-12-11 13:12:27 +0530

    Collect Animation  for Key

--- Assets/Scripts/Collectibles/CollectKey.cs
+++ Assets/Scripts/Collectibles/CollectKey.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class CollectKey : MonoBehaviour
 {
+    bool canAddScore;
+    Animator animator;
+    private void Start()
+    {
+        canAddScore = true;
+        animator = GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag=="Player")
+        if (canAddScore)
         {
-            ScoreControler scoreController = collision.gameObject.GetComponent<ScoreControler>();
-            scoreController.increaseScore();
-            Destroy(gameObject);
+            if (collision.gameObject.tag == "Player")
+            {
+                canAddScore = false;
+                animator.Play("Collected");
+                ScoreControler scoreController = collision.gameObject.GetComponent<ScoreControler>();
+                scoreController.increaseScore();
+                Invoke("DestroyKey", .6f);
+
+            }
         }
+    }
+
+    void DestroyKey()
+    {
+        Destroy(gameObject);
     }
 }
